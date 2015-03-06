096dea609227c217d0eb60a85c326f719692996e
Emir <emiratesbp@gmail.com>
2015-03-06 14:59:36 +0100

    First commit

---
+++ first/settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="28w289 Warrenville Rd"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Warrenville, IL 60555";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=6303932770;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
